52f8e585e16ba8c2063a46ae3e845cf736abcc3e
MMAndresC <mandrescasabon@hotmail.com>
2022-05-16 23:24:53 +0200

    Iteracion#2

--- basicfunctions.js
+++ basicfunctions.js
@@ -7,4 +7,19 @@ function sum(numberOne , numberTwo) {
 sum(10,9);
 
 
- 
+  //Iteracion #2
+  //Completa la función que tomando un array de strings como argumento devuelva el más largo, en caso de que dos strings tenga la misma longitud deberá devolver el primero.
+
+const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+
+function findLongestWord(param) {
+    let longestArray ='';
+    for(const element of param){
+        if (element.length > longestArray.length){
+            longestArray = element;
+        }
+    }    
+    return longestArray;
+}
+
+console.log(findLongestWord (avengers));
