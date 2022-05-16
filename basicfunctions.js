0e9128ede85023eeb6ad1901c2394fc011c50e5b
MMAndresC <mandrescasabon@hotmail.com>
2022-05-16 23:23:06 +0200

    Iteracion#1

---
+++ basicfunctions.js
@@ -0,0 +1,10 @@
+//Iteracion #1
+//Completa la función que tomando dos números como argumento devuelva el más alto.
+function sum(numberOne , numberTwo) {
+    (numberOne > numberTwo) ? console.log(numberOne) : console.log(numberTwo);
+}
+
+sum(10,9);
+
+
+ 
